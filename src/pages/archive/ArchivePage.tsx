3452aa4a53e7e4e61aa0bc611340815af3c743db
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-17 05:26:35 +0900

    fix: chip 컴포넌트 수정

--- src/pages/archive/ArchivePage.tsx
+++ src/pages/archive/ArchivePage.tsx
@@ -1,5 +1,21 @@
+import Layout from '../../layout/Layout';
+import Header from '../home/components/Header';
+import { Chip } from './components/chip';
+
 const ArchivePage = () => {
-  return <div>아카이브페이지</div>;
+  return (
+    <div>
+      <Header title="아카이빙" />
+      <Layout>
+        <div className="flex gap-2 pt-4.5 pb-7.5">
+          <Chip label="전체" isSelected={true} />
+          <Chip label="사랑" isSelected={false} />
+          <Chip label="용기" isSelected={false} />
+          <Chip label="솔직함" isSelected={false} />
+        </div>
+      </Layout>
+    </div>
+  );
 };
 
 export default ArchivePage;
