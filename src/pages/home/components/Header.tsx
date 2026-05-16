357a15c73b061e92178b7a74978bdb41db99aab3
ahyohyo <sywjsx0x@gmail.com>
2026-05-17 04:58:49 +0900

    feat: 헤더 컴포넌트 구현

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -1,22 +1,29 @@
 import { useNavigate } from 'react-router';
+import ArrowBack from '../../../shared/assets/ArrowBack.svg';
 
 interface HeaderProps {
-  title: string;
+  title?: string;
+  showBack?: boolean;
 }
 
-export default function Header({ title }: HeaderProps) {
+export default function Header({ title, showBack = true }: HeaderProps) {
   const navigate = useNavigate();
-  
+
   return (
-    <div>
-      <button
-        onClick={() => navigate(-1)}
-      >
-        뒤로가기 버튼
-      </button>
-      <span>
-        {title}
-      </span>
+    <div className="relative flex w-full h-[46px] items-center justify-center px-4">
+      {showBack && (
+        <button
+          onClick={() => navigate(-1)}
+          className="absolute left-4 flex items-center justify-center"
+        >
+          <img src={ArrowBack} alt="뒤로가기" />
+        </button>
+      )}
+      {title && (
+        <span className="text-black font-[Inter] text-[17px] font-semibold leading-[25.5px] tracking-[-0.732px]">
+          {title}
+        </span>
+      )}
     </div>
   );
 }
