8ea670ab3e00cf0bb1b2f1756b769699ffa030d1
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-17 03:41:31 +0900

    feat: Textarea 컴포넌트 스타일 적용

--- src/shared/components/Textarea.tsx
+++ src/shared/components/Textarea.tsx
@@ -6,7 +6,7 @@ interface TextareaProps {
 
 function Textarea({ value, onChange, maxLength = 50 }: TextareaProps) {
   return (
-    <div className="relative h-[348px] w-full rounded-[8px] bg-[#F7F7FA]">
+    <div className="border-background-gray relative h-[231px] w-[335px] rounded-[5px] border bg-white">
       <textarea
         value={value}
         onChange={(event) => onChange(event.target.value)}
@@ -14,8 +14,9 @@ function Textarea({ value, onChange, maxLength = 50 }: TextareaProps) {
         className="h-full w-full resize-none bg-transparent px-[28px] py-[28px] pb-[64px] text-[2rem] outline-none"
       />
 
-      <span className="absolute right-[28px] bottom-[28px] text-[1.8rem] text-[#999999]">
-        {value.length}/{maxLength}
+      <span className="absolute right-[20px] bottom-[20px] text-[1.8rem]">
+        <span className="text-text-primary">{value.length}</span>
+        <span className="text-sub-gray">/{maxLength}</span>
       </span>
     </div>
   );
